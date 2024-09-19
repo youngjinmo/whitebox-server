c57e750f12704b7d30ce4b55dc70155a24d24eb2
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-09-19 09:30:28 +0900

    Vue서버와의 통신을 위한 설정

---
+++ src/main/java/io/andy/shorten_url/config/WebConfig.java
@@ -0,0 +1,23 @@
+package io.andy.shorten_url.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:8080") // vue 서버
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
