youngjinmo/whitebox-spring

CommonRepository 인터페이스 추가

src/main/java/io/andy/shorten_url/common/CommonRepository.java
@@ -1,0 +1,11 @@
+package io.andy.shorten_url.common;
+
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommonRepository<Entity> {
+    Optional<List<Entity>> findByOrderByCreatedAtAsc(Pageable pageable);
+    Optional<List<Entity>> findByOrderByCreatedAtDesc(Pageable pageable);
+}

youngjinmo/whitebox-server

src/main/java/io/andy/shorten_url/common/CommonRepository.java
@@ -1,0 +1,11 @@
+package io.andy.shorten_url.common;
+
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommonRepository<Entity> {
+    Optional<List<Entity>> findAllByOrderByCreatedAtAsc(Pageable pageable);
+    Optional<List<Entity>> findAllByOrderByCreatedAtDesc(Pageable pageable);
+}