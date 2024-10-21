92dfbe40c4b75090fac8747f59c5fa1325bc3bb3
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-10-21 16:06:22 +0900

    Refactor EncodeUtil

--- src/main/java/io/andy/shorten_url/util/encrypt/EncodeUtil.java
+++ src/main/java/io/andy/shorten_url/util/encrypt/EncodeUtil.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 
 @Slf4j
 public final class EncodeUtil {
@@ -25,8 +24,17 @@ public final class EncodeUtil {
             // 인자를 byte 배열로 변환 후 해싱
             byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
 
-            // 해싱 결과를 Base64로 인코딩하여 반환
-            return Base64.getEncoder().encodeToString(hash);
+            // 해싱 결과를 문자열로 변환 후 반환
+            StringBuilder hexString = new StringBuilder();
+            for(byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                } else {
+                    hexString.append(hex);
+                }
+            }
+            return hexString.toString();
         } catch (NoSuchAlgorithmException e) {
             log.error("failed to encrypt word by {}", e.getMessage());
             throw new InternalServerException("FAILED TO ENCRYPT");
