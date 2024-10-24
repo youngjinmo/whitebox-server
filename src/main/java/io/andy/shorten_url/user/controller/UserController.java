1bc91ccb752a3eb84ac5bbc56dd552c96fe2b3d1
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-10-24 13:00:57 +0900

    Email 인증 구현

--- src/main/java/io/andy/shorten_url/user/controller/UserController.java
+++ src/main/java/io/andy/shorten_url/user/controller/UserController.java
@@ -1,16 +1,21 @@
 package io.andy.shorten_url.user.controller;
 
+import io.andy.shorten_url.exception.client.ForbiddenException;
 import io.andy.shorten_url.exception.client.UnauthorizedException;
+import io.andy.shorten_url.exception.server.InternalServerException;
 import io.andy.shorten_url.session.SessionService;
 import io.andy.shorten_url.user.dto.*;
 import io.andy.shorten_url.user.service.UserService;
-
 import io.andy.shorten_url.util.ClientMapper;
+import io.andy.shorten_url.util.mail.MailService;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,11 +28,17 @@ import java.util.Objects;
 public class UserController {
     private final UserService userService;
     private final SessionService sessionService;
+    private final MailService mailService;
 
     @Autowired
-    UserController(UserService userService, SessionService sessionService) {
+    public UserController(
+            UserService userService,
+            SessionService sessionService,
+            MailService mailService
+    ) {
         this.userService = userService;
         this.sessionService = sessionService;
+        this.mailService = mailService;
     }
 
     @PostMapping("/create")
@@ -48,9 +59,33 @@ public class UserController {
         String userAgent = request.getHeader("User-Agent");
 
         UserResponseDto user = userService.login(new UserLoginDto(username, ip, userAgent), password);
-        sessionService.setAttribute(request, user.id());
+        sessionService.setAuthSession(request, user.id());
 
         return user;
+    }
+
+    @PostMapping("/send-email-auth")
+    public ResponseEntity<Void> sendEmailAuth(HttpServletRequest request, @RequestParam String recipient) {
+        if (userService.isDuplicateUsername(recipient)) {
+            log.debug("[FORBIDDEN] Already existing user tried to send email authentication = {}", recipient);
+            throw new ForbiddenException("EMAIL DUPLICATED");
+        }
+        try {
+            String secretCode = this.mailService.sendMail(recipient);
+            sessionService.setMailAuthSession(request, secretCode);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("failed to sent mail = {}, error message={}", recipient, e.getMessage());
+            throw new InternalServerException("FAILED TO SENT MAIL");
+        }
+    }
+
+    @GetMapping("/verify-email-auth")
+    public boolean verifyEmailAuth(HttpServletRequest request, @RequestParam String secretCode) {
+        Object authSession = sessionService.getMailAuthSession(request);
+        if (Objects.isNull(authSession)) return false;
+        return authSession.equals(secretCode);
     }
 
     @DeleteMapping("/logout/{id}")
@@ -79,19 +114,19 @@ public class UserController {
 
     @PatchMapping("/{id}/username")
     public UserResponseDto UpdateUsername(HttpServletRequest request, @PathVariable("id") Long id, @RequestBody String username) {
-        validateSession(request);
+        validateSession(request, id);
         return userService.updateUsernameById(id, username);
     }
 
     @PatchMapping("/{id}/password")
     public UserResponseDto UpdatePassword(HttpServletRequest request, @PathVariable("id") Long id, @RequestBody String password) {
-        validateSession(request);
+        validateSession(request, id);
         return userService.updatePasswordById(id, password);
     }
 
     @DeleteMapping("/{id}")
     public void deleteUser(HttpServletRequest request, @PathVariable("id") Long id) {
-        validateSession(request);
+        validateSession(request, id);
 
         String clientIp = ClientMapper.parseClientIp(request);
         String userAgent = ClientMapper.parseUserAgent(request);
@@ -100,13 +135,9 @@ public class UserController {
         userService.deleteById(new UserDeleteDto(id, clientIp, userAgent));
     }
 
-    private void validateSession(HttpServletRequest request) {
-        if (Objects.isNull(sessionService.getAttribute(request))) {
-            String clientIp = ClientMapper.parseClientIp(request);
-            String userAgent = ClientMapper.parseUserAgent(request);
-
-            log.debug("invalidate session, clientIp={}, user-agent={}", clientIp, userAgent);
-            throw new UnauthorizedException("INVALIDATE SESSION");
-        }
+    private void validateSession(HttpServletRequest request, Long id) {
+        Object authSession = sessionService.getAuthSession(request);
+        if (Objects.isNull(authSession)) throw new UnauthorizedException();
+        if (authSession != id) throw new UnauthorizedException();
     }
 }
