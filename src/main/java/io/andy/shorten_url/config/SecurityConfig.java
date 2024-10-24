2dfde97c7b6d0d4ba71abde5a7fd677bcebdea7c
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-10-24 09:58:52 +0900

    Vue 통신위한 Cors 설정 추가

--- src/main/java/io/andy/shorten_url/config/SecurityConfig.java
+++ src/main/java/io/andy/shorten_url/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package io.andy.shorten_url.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(requests
                         -> requests.requestMatchers("/ping", "/user/create").permitAll()
                         .anyRequest().authenticated()
@@ -27,5 +32,18 @@ public class SecurityConfig {
                 .formLogin((form) -> form.loginPage("/user/login").permitAll())
                 .logout((logout) -> logout.logoutUrl("/user/logout").permitAll());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:8080"); // Vue.js 서버
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", configuration); // CORS 적용 범위 설정
+        return source;
     }
 }
