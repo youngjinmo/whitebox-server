7c50638a5796dabd78712b43f8bc8436b18c6547
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-10-26 16:38:23 +0900

    Refactor ObjectUtil

--- src/main/java/io/andy/shorten_url/util/ObjectUtil.java
+++ src/main/java/io/andy/shorten_url/util/ObjectUtil.java
@@ -5,31 +5,26 @@ import io.andy.shorten_url.exception.server.ObjectUtilException;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
 
 @Slf4j
 public class ObjectUtil {
-    public static <T> T fieldFilter(T target, String... fields) {
+    public static <T> T fieldFilter(T target, String[] excludeFields) {
+        if (Objects.isNull(target) || Objects.isNull(excludeFields) || excludeFields.length == 0) {
+            log.error("target class or fields cannot be null");
+            throw new IllegalArgumentException("target class or fields cannot be null");
+        }
         try {
-            List<String> excludeFields = Arrays.asList(fields);
-            T object = (T) target.getClass().getDeclaredConstructor().newInstance();
-            for (Field field : target.getClass().getDeclaredFields()) {
-                field.setAccessible(true);
-                if (!excludeFields.contains(field.getName())) {
-                    field.set(object, field.get(target));
-                }
+            Class<?> targetClass = target.getClass();
+            for (String field : excludeFields) {
+                Field fieldOfClass = targetClass.getDeclaredField(field);
+                fieldOfClass.setAccessible(true);
+                fieldOfClass.set(target, null);
             }
-            return object;
+            return (T) targetClass.cast(target);
         } catch (Exception e) {
             log.error("failed to filter field of {}.class. caused by {}", target, e.getMessage());
             throw new ObjectUtilException("FAILED TO FIELD FILTER");
         }
-    }
-
-    public static <T> T fieldFilter(Optional<T> target, String... fields) {
-        T object = target.orElse(null);
-        return fieldFilter(object, fields);
     }
 }
