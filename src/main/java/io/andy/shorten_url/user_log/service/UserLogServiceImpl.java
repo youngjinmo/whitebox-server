06c5edd2490e28d4d5f822ffd16310b8dd94fac3
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-11-12 20:39:26 +0900

    UserLogService 생성자 인자 리팩토링

--- src/main/java/io/andy/shorten_url/user_log/service/UserLogServiceImpl.java
+++ src/main/java/io/andy/shorten_url/user_log/service/UserLogServiceImpl.java
@@ -8,9 +8,9 @@ import io.andy.shorten_url.user_log.dto.UpdatePrivacyInfoDto;
 import io.andy.shorten_url.user_log.entity.UserLog;
 import io.andy.shorten_url.user_log.repository.UserLogRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,13 +20,9 @@ import java.util.List;
 @Slf4j
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class UserLogServiceImpl implements UserLogService {
-
-    @Autowired private final UserLogRepository userLogRepository;
-
-    public UserLogServiceImpl(UserLogRepository userLogRepository) {
-        this.userLogRepository = userLogRepository;
-    }
+    private final UserLogRepository userLogRepository;
 
     @Override
     public void putUserAccessLog(AccessInfoDto userAccessDto) {
@@ -44,6 +40,7 @@ public class UserLogServiceImpl implements UserLogService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<UserLog> getUserLogsByUserId(Long userId) {
         return userLogRepository.findByUserId(userId).orElseThrow(() -> {
             log.debug("not found user logs by userId={}", userId);
@@ -52,6 +49,7 @@ public class UserLogServiceImpl implements UserLogService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<UserLog> getUserLogsByMessage(UserLogMessage message) {
         return userLogRepository.findByMessage(message).orElseThrow(() -> {
             log.debug("not found user logs by message={}", message);
@@ -60,11 +58,13 @@ public class UserLogServiceImpl implements UserLogService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<UserLog> findAllUserLogs() {
         return userLogRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<UserLog> findLatestUserLogs(int limit) {
         return userLogRepository.findAllByOrderByCreatedAtDesc(PageRequest.of(0, limit)).orElseThrow(() -> new NotFoundException("NOT FOUND USER LOGS"));
     }
