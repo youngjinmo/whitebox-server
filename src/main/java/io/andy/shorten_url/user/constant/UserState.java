16923e78b38e67003d37ef0005ab7b99b493c48e
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-11-23 07:31:13 +0900

    UserState 주석 추가

--- src/main/java/io/andy/shorten_url/user/constant/UserState.java
+++ src/main/java/io/andy/shorten_url/user/constant/UserState.java
@@ -1,5 +1,8 @@
 package io.andy.shorten_url.user.constant;
 
 public enum UserState {
-    NEW, NORMAL, WITHDRAWN, DELETED
+    NEW,  // 신규 가입한 회원 상태 (아직 최초 로그인을 하지않음)
+    NORMAL, // 정상 회원 상태
+    WITHDRAWN, // 탈퇴 중 상태
+    DELETED // 삭제된 회원 상태
 }
