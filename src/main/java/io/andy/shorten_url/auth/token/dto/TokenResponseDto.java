0dfad806e36a0c29b6b95b02eaa6f03f5e1d79b9
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-12-05 19:17:53 +0900

    Refactor TokenResponseDto

--- src/main/java/io/andy/shorten_url/auth/token/dto/TokenResponseDto.java
+++ src/main/java/io/andy/shorten_url/auth/token/dto/TokenResponseDto.java
@@ -2,6 +2,12 @@ package io.andy.shorten_url.auth.token.dto;
 
 public record TokenResponseDto(String accessToken, String refreshToken) {
     public static TokenResponseDto build(String accessToken, String refreshToken) {
+        if (accessToken.isEmpty()) {
+            throw new IllegalArgumentException("Access token cannot be empty");
+        }
+        if (refreshToken.isEmpty()) {
+            throw new IllegalArgumentException("Refresh token cannot be empty");
+        }
        return new TokenResponseDto(accessToken, refreshToken);
     }
 }
